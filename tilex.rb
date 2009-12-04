ba5cf4c75bfb8cbc82fe1be716101f46eee6af98
jawbroken <jawbroken@gmail.com>
2009-12-05 02:53:24 +1100

    extract chunk data

--- tilex.rb
+++ tilex.rb
@@ -4,6 +4,24 @@ Dir['./data/*.sty'].each do |filename|
     next if file_type != "GBST"
     
     # apparently 700, not 600 as in the dox
-    version_code = f.read(2).unpack("S")
+    version_code = f.read(2).unpack("S")[0]
+    
+    # extract all chunks from file
+    chunks = []
+    chunk_type = f.read(4)
+    while(!chunk_type.nil?)
+      chunk_size = f.read(4).unpack("I")[0]
+      
+      p [chunk_type, chunk_size]
+      
+      chunk_data = f.read(chunk_size)
+            
+      chunks << [chunk_type, chunk_size, chunk_data]
+      
+      # set up next loop
+      chunk_type = f.read(4)
+    end
+    
+    exit
   end
 end
