1029c7e1557725e5eefceda9b95282b42d62e7cc
jawbroken <jawbroken@gmail.com>
2009-12-05 03:14:03 +1100

    extract tile data

--- tilex.rb
+++ tilex.rb
@@ -22,6 +22,30 @@ Dir['./data/*.sty'].each do |filename|
       chunk_type = f.read(4)
     end
     
+    processed = {}
+    
+    chunks.each do |type, size, data|
+      case type
+      when "TILE"
+        processed[:tile] = {}
+        index = 0
+        tile_num = 0
+        
+        while index < size
+          tile_dat = data[index,64**2]
+          processed[:tile][tile_num] = tile_dat
+          index += 64**2
+          tile_num += 1
+        end
+      when "PPAL"
+        
+      else
+        puts "ERROR: Unknown Chunk Type: #{type}"
+        exit
+      end
+      
+    end
+    
     exit
   end
 end
